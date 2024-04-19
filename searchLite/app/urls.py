17f60e43822e939e6ab23e39576232add26ef6e0
Sanket Dalvi <dalvi.sanket26@gmail.com>
2024-04-18 22:59:36 -0400

    upload view

--- searchLite/app/urls.py
+++ searchLite/app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('example/', views.example_view, name='example'),
     path('', views.example_view, name='example'),
+
+    path('upload/', views.upload_file, name='upload'),
 ]
--- searchLite/app/views.py
+++ searchLite/app/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+import os
 
 def example_view(request):
     
     # Perform your search logic here
     # For demonstration purposes, we'll just return the query
     return render(request, 'index.html')
+
+
+    
+def upload_file(request):
+    if request.method == 'POST' and request.FILES['file']:
+        file = request.FILES['file']
+        file_path = os.path.join(settings.BASE_DIR, 'corpus', file.name)
+        with open(file_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+        return render(request, 'upload.html', {'message': 'File uploaded successfully'})
+    return render(request, 'upload.html')
 
